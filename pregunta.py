ac1f3dcf436cef32b624be6fd0b5910a8fe7ff80
Esteban Rojas Henao <alto@conberojas.udea.red>
2023-03-16 15:49:06 -0500

    Upload preguntas

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,55 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+
+    # Verificacion tipo de datos
+
+    df['fecha_de_beneficio'] = pd.to_datetime(df.fecha_de_beneficio, dayfirst = True)
+
+    # Verificacion de rangos numericos y de fecha
+
+    # Manejo de categorias
+
+    # Registros duplicados
+
+    df = df.drop_duplicates().dropna()
+
+    # Unificacion de cadenas de texto
+
+    ## Sexo
+
+    df['sexo'] = df['sexo'].str.lower()
+
+    ## Tipo de emprendimiento
+
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+
+    ## Idea de negocio
+
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+
+    df['idea_negocio'] = df['idea_negocio'].replace(' |-', '_', regex=True)
+
+    ## Barrio
+
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].replace(' |-', '_', regex=True)
+
+    ## Linea de credito
+
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].replace(' |-', '_', regex=True)
+
+
+    ## Monto del credito
+
+    df['monto_del_credito'] = df['monto_del_credito'].replace('\$| |,|\.00', '', regex=True)
+
+    # Formato de fechas
+
+    #print(df['estrato'].value_counts())
+
+    #df.to_csv('Prueba.csv', index=False)
 
     return df
